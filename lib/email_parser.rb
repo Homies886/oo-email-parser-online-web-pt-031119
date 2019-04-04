a9a44a87fadd646c8e8e6733fc4b95897dc327d1
Nicholas Rodriguez <niko876@hotmail.com>
2019-04-04 16:44:14 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -12,9 +12,7 @@ class EmailParser
   end
   
   def parse
-    parsed = @email.split(", ").uniq || @email.split(" ").uniq
-    
-    
+    parsed = @email.split(", ").uniq || parsed = @email.split(" ").uniq
     
   end
   
